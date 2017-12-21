65e8dc4b9ffe3473962a336e37fbe87177fb0415
murmurmuk <yennanu@gmail.com>
2017-12-21 22:22:35 +0800

    Provider injections

--- app/src/main/java/murmur/daggerpratice/MainActivity.kt
+++ app/src/main/java/murmur/daggerpratice/MainActivity.kt
@@ -2,16 +2,21 @@ package murmur.daggerpratice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import javax.inject.Inject
+import javax.inject.Provider
 
 class MainActivity : AppCompatActivity() {
     @Inject
-    lateinit var coffee: Coffee
+    lateinit var coffeeProvider: Provider<Coffee>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerCoffeeShop.create().order(this)
-        coffee.show()
+        for (i in 1..3) {
+            Log.d("kanna", "order $i")
+            coffeeProvider.get().show()
+        }
     }
 }
