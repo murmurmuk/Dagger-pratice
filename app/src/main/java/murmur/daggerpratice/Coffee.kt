18c52da4db9149288b77786c084186fdccbaee87
murmurmuk <yennanu@gmail.com>
2017-12-21 22:13:52 +0800

    Lazy injections

--- app/src/main/java/murmur/daggerpratice/Coffee.kt
+++ app/src/main/java/murmur/daggerpratice/Coffee.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import javax.inject.Inject
 
 class Coffee @Inject constructor(){
+    init{
+        Log.d("kanna", "create coffee")
+    }
     fun show(){
         Log.d("kanna", "~coffee~")
     }
--- app/src/main/java/murmur/daggerpratice/MainActivity.kt
+++ app/src/main/java/murmur/daggerpratice/MainActivity.kt
@@ -2,16 +2,19 @@ package murmur.daggerpratice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.Lazy as DaggerLazy
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     @Inject
-    lateinit var coffee: Coffee
+    lateinit var coffee: DaggerLazy<Coffee>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerCoffeeShop.create().order(this)
-        coffee.show()
+        Log.d("kanna", "before get coffee")
+        coffee.get().show()
     }
 }
