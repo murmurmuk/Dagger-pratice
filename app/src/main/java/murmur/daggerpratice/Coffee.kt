37e701c00a8f0457a2ba69ae5261fbf54fd0a161
murmurmuk <yennanu@gmail.com>
2017-12-25 21:11:32 +0800

    BindsInstance

--- app/src/main/java/murmur/daggerpratice/Coffee.kt
+++ app/src/main/java/murmur/daggerpratice/Coffee.kt
@@ -1,46 +1,21 @@
 package murmur.daggerpratice
 
 import android.util.Log
-import dagger.Binds
 import dagger.Module
-import javax.inject.Inject
-import javax.inject.Qualifier
+import dagger.Provides
 
 @Module
-abstract class CoffeeModule {
-    @Binds
-    @Ice
-    abstract fun provideIceCoffee(coffee: IceCoffee): Coffee
-
-    @Binds
-    @Hot
-    abstract fun provideHotCoffee(coffee: HotCoffee): Coffee
-}
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Ice
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Hot
-
-open class Coffee {
-    open fun show() {
-        Log.d("kanna", "~coffee~")
+class CoffeeModule{
+    @Provides
+    fun provideCoffee(@UserName userName: String): Coffee{
+        return Coffee(userName)
     }
 }
 
-class IceCoffee @Inject constructor() : Coffee() {
-    override fun show() {
-        Log.d("kanna", "~ice coffee~")
+class Coffee (private val userName: String) {
+    fun show() {
+        Log.d("kanna", "$userName's ~coffee~")
     }
 }
 
-class HotCoffee @Inject constructor() : Coffee() {
-    override fun show() {
-        Log.d("kanna", "~hot coffee~")
-    }
-}
+annotation class UserName
--- app/src/main/java/murmur/daggerpratice/MainActivity.kt
+++ app/src/main/java/murmur/daggerpratice/MainActivity.kt
@@ -6,18 +6,17 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
     @Inject
-    @field:Hot
-    lateinit var coffee1: Coffee
+    lateinit var coffee: Coffee
 
-    @Inject
-    @field:Ice
-    lateinit var coffee2: Coffee
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DaggerCoffeeShop.create().order(this)
-        coffee1.show()
-        coffee2.show()
+        DaggerCoffeeShop
+                .builder()
+                .userName("kanna")
+                .build()
+                .order(this)
+        coffee.show()
     }
 }
