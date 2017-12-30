13bed3a668fa680965d6b9d2e72897dbc693b286
murmurmuk <yennanu@gmail.com>
2017-12-30 10:53:56 +0800

    ContributesAndroidInjector

--- app/src/main/java/murmur/daggerpratice/AppModule.kt
+++ app/src/main/java/murmur/daggerpratice/AppModule.kt
@@ -1,17 +1,10 @@
 package murmur.daggerpratice
 
-import android.app.Activity
-import dagger.Binds
 import dagger.Module
-import dagger.android.ActivityKey
-import dagger.android.AndroidInjector
-import dagger.multibindings.IntoMap
+import dagger.android.ContributesAndroidInjector
 
-@Module(subcomponents = [CoffeeShop::class])
+@Module
 abstract class AppModule {
-    @Binds
-    @IntoMap
-    @ActivityKey(MainActivity::class)
-    abstract fun bindMainActivityInjectFactory(builder: CoffeeShop.Builder)
-            : AndroidInjector.Factory<out Activity>
+    @ContributesAndroidInjector(modules = [CoffeeModule::class])
+    abstract fun CoffeeShop(): MainActivity
 }
